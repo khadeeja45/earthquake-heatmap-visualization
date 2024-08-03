9bc50f633ee24d777b0182c3750de2bc4e735918
Khadeeja Tul Qubra <ssskhadijatul@gmail.com>
2024-08-03 09:49:57 +0500

    heatmap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Heatmap from './components/Heatmap';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Heatmap />
+    </div>
+  );
+};
+
+export default App;
+
